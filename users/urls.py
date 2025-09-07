6fe052f4ed05516f61e900ad3b5c85e7b853a52a
Shuv1Wolf <mskuratik06@gmail.com>
2025-09-07 12:31:18 +0300

    small fix

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')), 
+    # Включить URL-адреса аутентификации по умолчанию.
+    path('', include('django.contrib.auth.urls')),
+    # Страница выхода из системы.
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html', next_page='learning_logs:index'), name='logout'),
+    # Страница регистрации.
     path('register/', views.register, name='register'),
 ]
