6ef1d5ee151b5e754b60e7f085efd2782b3e0f98
Shuv1Wolf <mskuratik06@gmail.com>
2025-09-07 12:35:56 +0300

    small fix

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Включить URL-адреса аутентификации по умолчанию.
     path('', include('django.contrib.auth.urls')),
     # Страница выхода из системы.
-    path('logout/', LogoutView.as_view(template_name='registration/logout.html', next_page='learning_logs:index'), name='logout'),
+    path('logged_out/', LogoutView.as_view(template_name='registration/logged_out.html', next_page='learning_logs:index'), name='logged_out'),
     # Страница регистрации.
     path('register/', views.register, name='register'),
 ]
